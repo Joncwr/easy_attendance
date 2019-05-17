d5e0992e1fa6da104f619a0df89f758996f39135
Jonathan Chua <Joncwrrr@gmail.com>
2019-05-17 12:24:18 +0800

    Added condition to attendance dates

--- lai_lai_server/telebot/menus/attendance/index.js
+++ lai_lai_server/telebot/menus/attendance/index.js
@@ -1,14 +1,21 @@
 const TelegrafInlineMenu = require('telegraf-inline-menu')
 const TelegramHelper = require('../../helpers/TelegramHelper')
 const { attendanceApi } = require('../../../routes/publicapi')
+const ObjectHelper = require('../../helpers/ObjectHelper')
 const { options } = require('./options')
 
 const attendance = new TelegrafInlineMenu(ctx => {
-  return 'Which date will you be taking attendance for!'
+  let { dates } = TelegramHelper.getDates()
+  if (ObjectHelper.isEmpty(dates)) {
+    return `I'm sorry, there are no available events yet! 😢`
+  }
+  else {
+    return 'Which date will you be taking attendance for! 🥳'
+  }
 })
 
 const dates = new TelegrafInlineMenu(ctx => {
-  return 'Great! Will you be attending?'
+  return 'Great! Will you be attending? We do hope you will! 🥰'
 })
 
 dates.manual('Yes', 'y')
