1fec06ead43c5129dd783903352afa9e11565b0d
Jonathan <joncwrrr@gmail.com>
2019-07-08 14:54:43 +0800

    edited chagne for getting prefix of EDWJ for daily devotion

--- lai_lai_server/telebot/helpers/DailyEDWJ.js
+++ lai_lai_server/telebot/helpers/DailyEDWJ.js
@@ -3,9 +3,9 @@ const schedule = require('node-schedule')
 
 // Initiate LS incase no key
 if (!localStorage.getItem('dailyEDWJ')) {
-  let day = moment().format('DD_MM_YY')
+  let day = moment().format('D_MM_YY')
   let dailyEDWJ = {
-    url: `https://s3-ap-southeast-1.amazonaws.com/bible-study-edwj/EDWJ_${day}.jpg`
+    url: `https://bible-study-edwj.s3-ap-southeast-1.amazonaws.com/EDWJ_${day}.jpg`
   }
 
   localStorage.setItem('dailyEDWJ', JSON.stringify(dailyEDWJ))
@@ -13,7 +13,7 @@ if (!localStorage.getItem('dailyEDWJ')) {
 
 schedule.scheduleJob('0 0 * * *', () => {
   let localItem = JSON.parse(localStorage.getItem('dailyEDWJ'))
-  let day = moment().format('DD_MM_YY')
-  localItem['url'] = `https://s3-ap-southeast-1.amazonaws.com/bible-study-edwj/EDWJ_${day}.jpg`
+  let day = moment().format('D_MM_YY')
+  localItem['url'] = `https://bible-study-edwj.s3-ap-southeast-1.amazonaws.com/EDWJ_${day}.jpg`
   localStorage.setItem('dailyEDWJ', JSON.stringify(localItem))
 }) // run everyday at midnight
